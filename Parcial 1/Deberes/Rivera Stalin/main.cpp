a51970fcdc36f584dc628b83f326df9f38095531
STALIN RIVERA VEGA <55595831+Stalin0@users.noreply.github.com>
2021-05-25 00:05:38 -0500

    v1.1

--- Parcial 1/Deberes/Rivera Stalin/main.cpp
+++ Parcial 1/Deberes/Rivera Stalin/main.cpp
@@ -1,31 +1,17 @@
 /*Universidad de las Fuerzas Armadas "ESPE"
 Titulo: Sobrecarga de Funciones
-Autor/es: Stalin Rivera 
+Autor/es: Stalin Rivera
 Fecha de Creación: 23/05/2021
 Fecha de Modificación: 24/05/2021
 Inginiería de Software
 Estructura de Datos*/
 
 #include <iostream>
-
+#include "Dividir.h"
 using namespace std;
 
-class division{
-	public:
-		double Calcular(double num1,double num2);
-		
-	private:
-		double calcular; 
-		
-};
-float Calcular(double num1,double num2);
-
-float Calcular(double num1,double num2){
-	return num1/num2;
-}
-
 int main(int arg, char** argv){
-	
+
 	double _num1;
 	double _num2;
 	cout<<"Ingrese el primer numero: ";
