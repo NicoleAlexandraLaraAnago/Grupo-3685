b75b7be47ead5971c03b3872a26185bf09d0d123
Shirley Otuna <84356035+Crystal2139@users.noreply.github.com>
2021-06-09 00:03:47 -0500

    Add files via upload

---
+++ Parcial 1/Deberes/Otuna Shirley/Pascal/Pascal.h
@@ -0,0 +1,14 @@
+
+class Pascal{
+	
+	public:
+	float factorial(float);
+	float combinacion(float,int);
+	int getNumA();
+	void setNumA(int);
+	Pascal(int);
+	void imprimir();
+	
+	private:
+	int numA;
+};
