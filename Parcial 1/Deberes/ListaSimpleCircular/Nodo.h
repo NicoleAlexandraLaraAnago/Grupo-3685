ccc223a160fe87ccb00a89e91021acaef35f8a7f
Shirley Otuna <84356035+Crystal2139@users.noreply.github.com>
2021-06-17 20:25:43 -0500

    Update Nodo.h

--- Parcial 1/Deberes/ListaSimpleCircular/Nodo.h
+++ Parcial 1/Deberes/ListaSimpleCircular/Nodo.h
@@ -1,12 +1,13 @@
-#include <iostream>
+/*Universidad de las Fuerzas Armadas ESPE
+Título: Lista Simple Circular
+#Grupo
+Autor/es: Kevin Condor, Nicole Lara, Cristian Maranje, Stalin Rivera, Shirley Otuna
+Fecha de creacíon: 16/06/2021
+Fecha de modificación: 17/06/2021
+Ingeniería en Software
+Estructura de Datos*/
 
-/*
-UNIVERSIDAD DE LAS FUERZAS ARMADAS ESPE							   
-Titulo: Listas simples 
-Autor: Cristian maranje											       
-Fecha: 08/06/2021													   
-Fecha de modificacion: 16/06/2021								   
-*/
+#include <iostream>
 
 class Nodo
 {
